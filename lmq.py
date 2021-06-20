2c3e2524c96616f9d78f42b56a58615b79b4b7fb
zjxlmq <83770885+zjxlmq@users.noreply.github.com>
2021-06-20 12:49:00 +0800

    Update lmq.py

--- lmq.py
+++ lmq.py
@@ -1,11 +1,11 @@
 import random
 keep = random.randint(1,100)
-print("这是我搭建的游戏")
-lmq= int(input("输入你想要输入的值"))
+print("…这是我搭建的游戏…")
+lmq= int(input("输入你想要输入的值:"))
 guess = int(lmq)
 while guess !=int(keep):
     print("猜错了，重新猜")
-    lmq = input("重新输入值")
+    lmq = input("重新输入值:")
     guess=int(lmq)
     if guess ==int(keep):
         print("猜对了")
